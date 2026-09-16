360254f97509faa54684e8e31d13f8c6d65a1794
Jeffergs <jeffersongs1995@gmail.com>
2026-09-16 07:09:33 -0300

    chore: adicionar conexão com Postgres e correção da repository

--- src/main/java/com/kifeito/user/Repository/UserRepository.java
+++ src/main/java/com/kifeito/user/Repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.kifeito.user.Repository;
 
+import com.kifeito.user.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserRepository,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
